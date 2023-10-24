8decb138062b09561bfd333758d8a46ffa6242d6
Fares Emad <faresemadx@gmail.com>
2023-10-25 00:06:02 +0200

    feat: add `pre-commit`

--- scorify/wsgi.py
+++ scorify/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scorify.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scorify.settings")
 
 application = get_wsgi_application()
